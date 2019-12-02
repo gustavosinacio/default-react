cc7bd90a58ace8ff5d3fcc3b9d431b455266d9fa
Gustavo Inacio <gsinacio94@gmail.com>
2019-12-02 18:34:40 -0300

    minor

--- .huskyrc.js
+++ .huskyrc.js
@@ -5,13 +5,13 @@ const postCommit = arr => arr.join(' && ')
 module.exports = {
   "hooks": {
     "pre-commit": preCommit([
-      "echo '--------------------------------------------|'",
-      "echo \"\n---  \\e[1m\\033[32mPre-commit =>\\e[0m executes: \\e[4m\\033[33mlint staged\\e[0m ---|\n\"",
+      "echo '--------------------------------------------'",
+      "echo \"\n---  \\e[1m\\033[32mPre-commit =>\\e[0m executes: \\e[4m\\033[33mlint staged\\e[0m ---\n\"",
       "lint-staged"
 
     ]),
     "post-commit": postCommit([
-      "echo '--------------------------------------------|'",
+      "echo '--------------------------------------------'",
     ]),
     "pre-push": "echo \"\n---  \\e[1m\\033[34mPre-push =>\\e[0m executes: \\e[4m\\033[33mempty\\e[0m ---\n\""
   }
