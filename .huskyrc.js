d0999af1045c52a89697bef9c4ed663387f54f42
Gustavo Inacio <gsinacio94@gmail.com>
2019-12-02 18:34:55 -0300

    minor

--- .huskyrc.js
+++ .huskyrc.js
@@ -6,7 +6,7 @@ module.exports = {
   "hooks": {
     "pre-commit": preCommit([
       "echo '--------------------------------------------'",
-      "echo \"\n---  \\e[1m\\033[32mPre-commit =>\\e[0m executes: \\e[4m\\033[33mlint staged\\e[0m ---\n\"",
+      "echo \"\n---  \\e[1m\\033[32mPre-commit =>\\e[0m executes: \\e[4m\\033[33mlint staged\\e[0m ---\"",
       "lint-staged"
 
     ]),
