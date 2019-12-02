ba0260f1de3e016c4828672d035b44dd4a196bf3
Gustavo Inacio <gsinacio94@gmail.com>
2019-12-02 18:42:39 -0300

    colors

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,12 +3,11 @@ const postCommit = arr => arr.join(' && ')
 const prePush = arr => arr.join(' && ')
 const postPush = arr => arr.join(' && ')
 
-
 module.exports = {
   "hooks": {
     "pre-commit": preCommit([
       "echo '\\e[1m\\033[32m--------------------------------------------\\e[0m'",
-      "echo \"\n---  \\e[1m\\033[32mPre-commit =>\\e[0m executes: \\e[4m\\033[33mlint staged\\e[0m ---\"",
+      "echo \"---  \\e[1m\\033[32mPre-commit =>\\e[0m executes: \\e[4m\\033[33mlint staged\\e[0m ---\"",
       "lint-staged"
 
     ]),
@@ -17,7 +16,7 @@ module.exports = {
     ]),
     "pre-push": prePush([
       "echo '\\e[1m\\033[34m--------------------------------------------\\e[0m'",
-      "echo \"\n---  \\e[1m\\033[34mPre-push =>\\e[0m executes: \\e[4m\\033[33mempty\\e[0m ---\n\""
+      "echo \"---  \\e[1m\\033[34mPre-push =>\\e[0m executes: \\e[4m\\033[33mempty\\e[0m ---\n\""
     ]),
     "post-push": postPush([
       "echo '\n\\e[1m\\033[34m--------------------------------------------\\e[0m'",
