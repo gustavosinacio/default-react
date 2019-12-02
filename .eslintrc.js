5d8f7031a2cd12af1e67c61ccd4332381ed4d6b6
Gustavo Inacio <gsinacio94@gmail.com>
2019-12-02 17:01:14 -0300

    fixes package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,15 @@ module.exports = {
     },
     "extends": [
         "plugin:react/recommended",
-        "airbnb"
+        "airbnb",
+        'prettier',
+        'prettier/react'
     ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
+    parser: 'babel-eslint',
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
@@ -19,8 +22,18 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react", 'prettier', 'react-hooks'
     ],
-    "rules": {
-    }
+    rules: {
+        "prettier/prettier": "error",
+        "react/jsx-filename-extension": [
+            'warn', 
+            { extensions: ['.jsx', '.js'] }
+        ],
+        'import/prefer-default-export': 'off',
+        'no-param-reassign': 'off',
+        'no-console': ['error', { allow: ['tron'] }], // this is for when using reactotron
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps':'warn'
+    },
 };
